452c22da6c9f36e3aa572d5d96f4bf18c9a70187
Mary Selig <seligmar@gmail.com>
2019-01-18 19:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName !== item)  {return "That item is not in your cart." }
     if (cart[i].itemName === item) { 
    var removeItem = cart.splice(i, 1);  
   return cart; 
 }
+else if (cart[i].itemName !== item)  {return "That item is not in your cart." }
 } 
 }
    
