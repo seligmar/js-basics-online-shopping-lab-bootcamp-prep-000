5bd627db4a1086edad80fc6095f51b8ed182804b
Mary Selig <seligmar@gmail.com>
2019-01-18 19:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
    var removeItem = cart.splice(i, 1);  
   return cart; 
 }  } 
-if (cart[i].itemName === undefined) {return "That item is not in your cart." }
+if (cart[i].itemName !== undefined) {return "That item is not in your cart." }
 } }
 
 function placeOrder(cardNumber) {
